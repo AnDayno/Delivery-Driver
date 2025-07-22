bcefba44bf7a36438368062e280f1d5ce12dd152
An <162073486+AnDayno@users.noreply.github.com>
2025-07-23 02:38:19 +0800

    DD: Added boostSpeed and SlowSpeed when bumping into things
    Also made changes to:
    - Layer Sorting
    - Added Pill sprite

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,15 +5,10 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
@@ -21,5 +16,21 @@ public class Driver : MonoBehaviour
 
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Obstacle"))
+        {
+            moveSpeed = slowSpeed;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("BoostPill"))
+        {
+            moveSpeed = boostSpeed;
+        }
     }
 }
